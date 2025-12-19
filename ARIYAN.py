8c404916d4ed6be0772244ff3733da2c54323464
ariyansabbirxo <ariyansabbir.xo@gmail.com>
2025-12-19 21:23:16 +0600

    Update ARIYAN.py

--- ARIYAN.py
+++ ARIYAN.py
@@ -1,1 +1,6 @@
-import ariyan
+import ctypes
+my_lib = ctypes.CDLL('./ariyan.cpython-313.so')
+my_lib.add.argtypes = (ctypes.c_int, ctypes.c_int)
+my_lib.add.restype = ctypes.c_int
+result = my_lib.add(5, 7)
+print("Result of add(5, 7) is:", result)
